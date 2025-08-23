98b24fb4c4b8b507532cda82e71aa7d5998939ea
Purvi Sahu <161992833+purvisahu2004@users.noreply.github.com>
2025-08-23 18:30:30 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,6 @@
+#py program to check whether Character is a vowel or consonant
+ch = input("enter any alphabet: ")
+if (ch >='a' and ch<='z') or (ch>='A' and ch<='Z'):
+    print("it is an alphabet")
+else:
+    print("no not an alphabet")
---
+++ problem2.py
@@ -0,0 +1,7 @@
+#Program to print ASCII value of a character
+ch = input("Enter any alphabet: ")
+print(ord(ch))
+
+#Program to print character from ASCII value of a character
+num = int(input("Enter any num between 65 and 112: "))
+print(chr(num))
